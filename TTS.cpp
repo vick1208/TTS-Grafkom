e2791c11018e0558cf77b57223aad9f6906a13fa
vick1208 <65887817+vick1208@users.noreply.github.com>
2020-06-17 11:58:40 +0700

    Add files via upload

--- TTS.cpp
+++ TTS.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <GL/freeglut.h>
+#include <math.h>
 void init() {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glMatrixMode(GL_PROJECTION);
@@ -8,7 +9,27 @@ void init() {
 }
 void disp() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-1.0f, -1.0f);
+	glVertex2f(-1.0f, 485.0f);
+	glVertex2f(645.0f, 485.0f);
+	glVertex2f(645.0f, -1.0f);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2f(10.0f, 600.0f);
+	glVertex2f(200.0f, 400.0f);
+	glVertex2f(510.0f, 299.9f);
+	glVertex2f(520.0f, 298.8f);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(11.0f, 601.0f);
+	glVertex2f(201.0f, 401.0f);
+	glVertex2f(511.0f, 300.9f);
+	glVertex2f(521.0f, 299.8f);
+	glEnd();
 	glFlush();
 }
 int main(int argc, char** argv) {
